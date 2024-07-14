59d5f94b503823cacf976c296c0c1bcc857cec4f
austinallen <austintallen07@gmail.com>
2024-07-14 18:32:57 -0400

    fixed an import issue within the server.

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const authRouter = require('./routes/auth');
 const tripRouter = require('./routes/trips');
 const flightRouter = require('./routes/flights');
-const googlePlacesRouter = require('./utils'); // Add this line
+const googlePlacesRouter = require('./routes/googlePlaces'); // Import the Google Places route
 
 require('dotenv').config();
 
@@ -16,19 +16,21 @@ const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/yourdbnam
 mongoose
   .connect(mongoURI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB', err));
 
 // CORS configuration
-app.use(cors({
-  origin: 'http://localhost:3001', // Replace with your frontend URL
-  credentials: true, // This allows cookies to be sent with the requests
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:3001', // Replace with your frontend URL
+    credentials: true, // This allows cookies to be sent with the requests
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 
 app.use(bodyParser.json());
 
